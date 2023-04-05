e04112de8e60cb1df3b3b7668b1de8f14d58ef3f
johan-stph <111685646+johan-stph@users.noreply.github.com>
2023-04-05 16:44:38 +0200

    Initial commit

---
+++ src/main/java/com/kit/springtemplatewithgooglemaven/SpringTemplateWithGoogleMavenApplication.java
@@ -0,0 +1,13 @@
+package com.kit.springtemplatewithgooglemaven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTemplateWithGoogleMavenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringTemplateWithGoogleMavenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kit/springtemplatewithgooglemaven/SpringTemplateWithGoogleMavenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kit.springtemplatewithgooglemaven;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTemplateWithGoogleMavenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
